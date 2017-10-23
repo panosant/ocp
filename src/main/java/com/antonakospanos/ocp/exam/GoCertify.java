2d32695cdb574e11fa1ab38e3783cbbf77ff07a7
panos.antonakos <panos.antonakos@upstreamsystems.com>
2017-10-23 20:54:42 +0300

    CHG: Upgraded to J9

---
+++ src/main/java/com/antonakospanos/ocp/exam/GoCertify.java
@@ -0,0 +1,9 @@
+package com.antonakospanos.ocp.exam;
+
+public class GoCertify {
+
+    public static void main(String[] args) {
+        int number = 0b1010_1011_0111;
+        System.out.println("Number: " + number);
+    }
+}
